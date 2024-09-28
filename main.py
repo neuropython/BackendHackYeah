9f9b89e773424a8300492ebc2fddc9605241e012
Damian <damianpietron007@gmail.com>
2024-09-28 16:14:04 +0200

    Connect to mongoDb

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+import pymongo
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb+srv://fastapi:123fastapi@hackyeah-db.3xvq7.mongodb.net/?retryWrites=true&w=majority&appName=hackyeah-db"
+
 
 app = FastAPI()
+
+client = MongoClient(MONGO_URI)
+db = client["project"]
+
 
 @app.get("/")
 def root():
     return {"message": "Hello, World!"}
-
-@app.post("/items/")
-def create_item(item: dict):
-    # Process the item data
-    return {"message": "Item created successfully"}
